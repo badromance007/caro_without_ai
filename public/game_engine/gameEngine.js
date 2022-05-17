9bee8ae39116acf7ed6af28b4b8f11997869d860
unknown <nguyenpeter.edu@gmail.com>
2022-05-17 09:10:15 +0700

    remove unused code

--- public/game_engine/gameEngine.js
+++ public/game_engine/gameEngine.js
@@ -5,32 +5,8 @@ COMBINATIONS
 */
 var Combinations = function() {
     var win = [[1, 1, 1, 1, 1]];
-    var unCovered4 = [[0, 1, 1, 1, 1, 0]];
-    var unCovered3 = [
-        [0, 1, 1, 1, 0, 0], [0, 0, 1, 1, 1, 0],
-        [0, 1, 0, 1, 1, 0], [0, 1, 1, 0, 1, 0]
-    ];
-    var unCovered2 = [
-        [0, 0, 1, 1, 0, 0], [0, 1, 0, 1, 0, 0],
-        [0, 0, 1, 0, 1, 0], [0, 1, 1, 0, 0, 0],
-        [0, 0, 0, 1, 1, 0], [0, 1, 0, 0, 1, 0]
-    ];
-    var covered4 = [
-        [-1, 1, 0, 1, 1, 1], [-1, 1, 1, 0, 1, 1],
-        [-1, 1, 1, 1, 0, 1], [-1, 1, 1, 1, 1, 0],
-        [0, 1, 1, 1, 1, -1], [1, 0, 1, 1, 1, -1],
-        [1, 1, 0, 1, 1, -1], [1, 1, 1, 0, 1, -1]
-    ];
-    var covered3 = [
-        [-1, 1, 1, 1, 0, 0], [-1, 1, 1, 0, 1, 0],
-        [-1, 1, 0, 1, 1, 0], [0, 0, 1, 1, 1, -1],
-        [0, 1, 0, 1, 1, -1], [0, 1, 1, 0, 1, -1],
-        [-1, 1, 0, 1, 0, 1, -1], [-1, 0, 1, 1, 1, 0, -1],
-        [-1, 1, 1, 0, 0, 1, -1], [-1, 1, 0, 0, 1, 1, -1]
-    ];
-
     (function() { //add same combinations for another player
-        var allCombos = [win, unCovered4, unCovered3, unCovered2, covered4, covered3];
+        var allCombos = [win];
         for (var k = 0; k < allCombos.length; k++) {
             var temp = [];
             for (var j = 0; j < allCombos[k].length; j++) {
@@ -45,46 +21,8 @@ var Combinations = function() {
         }
     }());
 
-    var valueCombo = function(w, u2, u3, u4, c3, c4) {
+    var valueCombo = function(w) {
         if (w > 0)            return 1000000000;
-        if (u4 > 0)           return 100000000;
-        if (c4 > 1)           return 10000000;
-        if (u3 > 0 && c4 > 0) return 1000000;
-        if (u3 > 1)           return 100000;
-
-        if (u3 == 1) {
-            if (u2 == 3)        return 40000;
-            if (u2 == 2)        return 38000;
-            if (u2 == 1)        return 35000;
-            return 3450;
-        }
-
-        if (c4 == 1) {
-            if (u2 == 3)        return 4500;
-            if (u2 == 2)        return 4200;
-            if (u2 == 1)        return 4100;
-            return 4050;
-        }
-
-        if (c3 == 1) {
-            if (u2 == 3)        return 3400;
-            if (u2 == 2)        return 3300;
-            if (u2 == 1)        return 3100;
-        }
-
-        if (c3 == 2) {
-            if (u2 == 2)        return 3000;
-            if (u2 == 1)        return 2900;
-        }
-
-        if (c3 == 3) {
-            if (u2 == 1)        return 2800;
-        }
-
-        if (u2 == 4)          return 2700;
-        if (u2 == 3)          return 2500;
-        if (u2 == 2)          return 2000;
-        if (u2 == 1)          return 1000;
         return 0;
     };
 
@@ -122,27 +60,8 @@ var Combinations = function() {
                 w++;
                 continue;
             }
-            if (isAnyInArrays(covered4, allArr[i])) {
-                c4++;
-                continue;
-            }
-            if (isAnyInArrays(covered3, allArr[i])) {
-                c3++;
-                continue;
-            }
-            if (isAnyInArrays(unCovered4, allArr[i])) {
-                u4++;
-                continue;
-            }
-            if (isAnyInArrays(unCovered3, allArr[i])) {
-                u3++;
-                continue;
-            }
-            if (isAnyInArrays(unCovered2, allArr[i])) {
-                u2++;
-            }
         }
-        return valueCombo(w, u2, u3, u4, c3, c4);
+        return valueCombo(w);
     };
     return combinations;
 };
@@ -325,10 +244,12 @@ ready(() => {
         if (index === "O"){
             logic = new Logic();
             currentSide = -1;
+            document.getElementById("message").textContent = "O turn";
         }
         if (index === "X"){
             logic = new Logic(1);
             currentSide = 1;
+            document.getElementById("message").textContent = "X turn";
         }
         document.getElementById("check").checked = false;
     }
